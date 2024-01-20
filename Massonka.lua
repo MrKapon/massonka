c24f0f79cbc2a1dc633a95f481f97a17b47251ce
MrKapon <114702744+MrKapon@users.noreply.github.com>
2024-01-21 02:37:18 +0300

    Add files via upload

--- Massonka.lua
+++ Massonka.lua
@@ -12,13 +12,13 @@ u8 = encoding.UTF8
 
 update_state = false
 
-local script_vers = 2
-local script_vers_text = "1.1"
+local script_vers = 3
+local script_vers_text = "1.3"
 
 local update_url = "https://raw.githubusercontent.com/MrKapon/massonka/main/update.ini" -- тут тоже свою ссылку
 local update_path = getWorkingDirectory() .. "/update.ini" -- и тут свою ссылку
 
-local script_url = "https://raw.githubusercontent.com/MrKapon/massonka/main/Massonka.lua" -- тут свою ссылку
+local script_url = "https://raw.githubusercontent.com/MrKapon/massonka/main/Massonka.lua?raw=true" -- тут свою ссылку
 local script_path = thisScript().path
 
 local directIni = "massonka.ini"
